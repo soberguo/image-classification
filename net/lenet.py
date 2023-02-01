c5dbbbd67af69a4cbda63010adc50392a9fb7e8b
soberguo <77473168+soberguo@users.noreply.github.com>
2023-02-01 21:02:07 +0800

    Create lenet.py

---
+++ net/lenet.py
@@ -0,0 +1,36 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+
+#n=(w-f+2p)/s+1___w图像宽高，f卷积核大小，p是padding，s步长
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet,self).__init__()
+        self.conv1=nn.Conv2d(3,16,5)
+        self.pool1=nn.MaxPool2d(2,2)
+        self.conv2=nn.Conv2d(16,32,5)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.fc1=nn.Linear(32*5*5,120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 120)
+
+    def forward(self,x):
+        x=self.conv1(x)           #input(3,32,32) output(16,28,28)
+        x=F.relu(x)
+        x=self.pool1(x)           #output(16,14,14)
+        x = self.conv2(x)         #output(32,10,10)
+        x = F.relu(x)
+        x = self.pool2(x)         #output(32,5,5)
+        x=x.view(-1,32*5*5)       #output(35*5*5)
+        x=F.relu(self.fc1(x))     #output(120)
+        x = F.relu(self.fc2(x))   #output(84)
+        x=self.fc3(x)             #output(10)
+        return x
+
+if __name__=='__main__':
+    model=LeNet()
+    input=torch.rand(32,3,32,32)
+    print(model)
+    output=model(input)
